8f6e60785e31a07d23348c069594645bc35e210d
dzoanka <joanna.lenkiewicz@gmail.com>
2023-12-13 17:54:43 +0100

    Reformat predict function

--- cellmaps_vnn/predict.py
+++ cellmaps_vnn/predict.py
@@ -8,6 +8,7 @@ from torch.autograd import Variable
 from cellmaps_utils import constants
 
 from cellmaps_vnn import util
+from cellmaps_vnn.exceptions import CellmapsvnnError
 
 logger = logging.getLogger(__name__)
 
@@ -75,8 +76,8 @@ class VNNPredict:
                          self._theargs.result, cell_features)
 
         except Exception as e:
-            logger.error(f"Error in prediction: {e}")
-            raise
+            logger.error(f"Error in prediction flow: {e}")
+            raise CellmapsvnnError(f"Encountered problem in prediction flow: {e}")
 
     def _prepare_predict_data(self, test_file, cell2id_mapping_file, zscore_method, std_file):
         cell2id_mapping = util.load_mapping(cell2id_mapping_file, 'cell lines')
@@ -104,64 +105,153 @@ class VNNPredict:
             label.append([float(row[2])])
         return feature, label
 
-    def predict(self, predict_data, gene_dim, model_file, hidden_folder, batch_size, result_file, cell_features):
-        feature_dim = gene_dim
+    def predict(self, predict_data, model_file, hidden_folder, batch_size, result_file, cell_features):
+        """
+        Perform prediction using the trained model.
 
-        model = torch.load(model_file, map_location='cuda:%d' % self._theargs.cuda)
-        predict_feature, predict_label = predict_data
-        predict_label_gpu = predict_label.cuda(self._theargs.cuda)
+        :param predict_data: Tuple of features and labels for prediction.
+        :param model_file: Path to the trained model file.
+        :param hidden_folder: Directory to store hidden layer outputs.
+        :param batch_size: Size of each batch for prediction.
+        :param result_file: File path prefix for storing prediction results.
+        :param cell_features: Additional cell features for prediction.
+        """
+        try:
+            model = self._load_model(model_file)
+            test_loader = self._create_data_loader(predict_data, batch_size)
+            test_predict, saved_grads = self._predict(model, test_loader, cell_features, hidden_folder, result_file)
 
+            predict_label_gpu = predict_data[1].cuda(self._theargs.cuda)
+            test_corr = util.pearson_corr(test_predict, predict_label_gpu)
+            logger.info(f"Test correlation {model.root}: {test_corr:.4f}")
+
+            np.savetxt(f'{result_file}.txt', test_predict.cpu().numpy(), '%.4e')
+
+        except Exception as e:
+            logger.error(f"Prediction error: {e}")
+            raise CellmapsvnnError(f"Encountered problem in prediction: {e}")
+
+    def _load_model(self, model_file):
+        """
+        Load the trained model for prediction.
+
+        :param model_file: Path to the trained model file.
+        :return: Loaded model.
+        """
+        model = torch.load(model_file, map_location=f'cuda:{self._theargs.cuda}')
         model.cuda(self._theargs.cuda)
         model.eval()
+        return model
 
-        test_loader = du.DataLoader(du.TensorDataset(predict_feature, predict_label), batch_size=batch_size,
-                                    shuffle=False)
+    def _create_data_loader(self, predict_data, batch_size):
+        """
+        Create a DataLoader for the prediction data.
 
+        :param predict_data: Tuple of features and labels for prediction.
+        :param batch_size: Size of each batch for prediction.
+        :return: DataLoader for the prediction data.
+        """
+        predict_feature, predict_label = predict_data
+        return du.DataLoader(du.TensorDataset(predict_feature, predict_label), batch_size=batch_size, shuffle=False)
+
+    def _predict(self, model, data_loader, cell_features, hidden_folder, result_file):
+        """
+        Run the prediction process and save outputs.
+
+        :param model: Trained model for prediction.
+        :param data_loader: DataLoader containing the prediction data.
+        :param cell_features: Additional cell features for prediction.
+        :param hidden_folder: Directory to store hidden layer outputs.
+        :param result_file: File path prefix for storing prediction results.
+        :return: Tuple of prediction results and saved gradients.
+        """
         test_predict = torch.zeros(0, 0).cuda(self._theargs.cuda)
-        hidden_embeddings_map = {}
-
         saved_grads = {}
 
-        def save_grad(element):
+        for i, (inputdata, labels) in enumerate(data_loader):
+            cuda_features = self._process_input(inputdata, cell_features)
+            aux_out_map, hidden_embeddings_map = model(cuda_features)
+            test_predict = torch.cat([test_predict, aux_out_map['final'].data], dim=0) if test_predict.size()[0] else \
+            aux_out_map['final'].data
+
+            self._save_hidden_outputs(hidden_embeddings_map, hidden_folder)
+            self._register_gradient_hooks(hidden_embeddings_map, saved_grads)
+            self._backpropagate(aux_out_map)
+
+            self._save_gradients(cuda_features, result_file)
+            self._save_hidden_gradients(saved_grads, hidden_folder)
+
+        return test_predict, saved_grads
+
+    def _process_input(self, inputdata, cell_features):
+        """
+        Process input data for the model.
+
+        :param inputdata: Input data for the model.
+        :param cell_features: Additional cell features for prediction.
+        :return: Processed features as CUDA variables.
+        """
+        features = util.build_input_vector(inputdata, cell_features)
+        return Variable(features.cuda(self._theargs.cuda), requires_grad=True)
+
+    def _save_hidden_outputs(self, hidden_embeddings_map, hidden_folder):
+        """
+        Save outputs from hidden layers.
+
+        :param hidden_embeddings_map: Dictionary of hidden layer outputs.
+        :param hidden_folder: Directory to save hidden layer outputs.
+        """
+        for element, hidden_map in hidden_embeddings_map.items():
+            hidden_file = os.path.join(hidden_folder, element + '.hidden')
+            with open(hidden_file, 'ab') as f:
+                np.savetxt(f, hidden_map.data.cpu().numpy(), '%.4e')
+
+    def _register_gradient_hooks(self, hidden_embeddings_map, saved_grads):
+        """
+        Register gradient hooks to save gradients of hidden layers.
+
+        :param hidden_embeddings_map: Dictionary of hidden layer outputs.
+        :param saved_grads: Dictionary to store saved gradients.
+        """
+
+        def save_grad(elem):
             def savegrad_hook(grad):
-                saved_grads[element] = grad
+                saved_grads[elem] = grad
 
             return savegrad_hook
 
-        for i, (inputdata, labels) in enumerate(test_loader):
-            features = util.build_input_vector(inputdata, cell_features)
-            cuda_features = Variable(features.cuda(self._theargs.cuda), requires_grad=True)
+        for element, _ in hidden_embeddings_map.items():
+            hidden_embeddings_map[element].register_hook(save_grad(element))
 
-            aux_out_map, hidden_embeddings_map = model(cuda_features)
+    def _backpropagate(self, aux_out_map):
+        """
+        Perform backpropagation.
 
-            if test_predict.size()[0] == 0:
-                test_predict = aux_out_map['final'].data
-            else:
-                test_predict = torch.cat([test_predict, aux_out_map['final'].data], dim=0)
+        :param aux_out_map: Auxiliary output map from the model.
+        """
+        aux_out_map['final'].backward(torch.ones_like(aux_out_map['final']))
 
-            for element, hidden_map in hidden_embeddings_map.items():
-                hidden_file = os.path.join(hidden_folder, element + '.hidden')
-                with open(hidden_file, 'ab') as f:
-                    np.savetxt(f, hidden_map.data.cpu().numpy(), '%.4e')
+    def _save_gradients(self, cuda_features, result_file):
+        """
+        Save gradients for each feature.
 
-            for element, _ in hidden_embeddings_map.items():
-                hidden_embeddings_map[element].register_hook(save_grad(element))
+        :param cuda_features: CUDA features variable.
+        :param result_file: File path prefix for storing gradients.
+        """
+        for i in range(len(cuda_features[0, 0, :])):
+            feature_grad = cuda_features.grad.data[:, :, i]
+            grad_file = f'{result_file}_feature_grad_{i}.txt'
+            with open(grad_file, 'ab') as f:
+                np.savetxt(f, feature_grad.cpu().numpy(), '%.4e', delimiter='\t')
 
-            aux_out_map['final'].backward(torch.ones_like(aux_out_map['final']))
+    def _save_hidden_gradients(self, saved_grads, hidden_folder):
+        """
+        Save the gradients of the hidden layer outputs.
 
-            feature_grad = torch.zeros(0, 0).cuda(self._theargs.cuda)
-            for i in range(len(cuda_features[0, 0, :])):
-                feature_grad = cuda_features.grad.data[:, :, i]
-                grad_file = result_file + '_feature_grad_' + str(i) + '.txt'
-                with open(grad_file, 'ab') as f:
-                    np.savetxt(f, feature_grad.cpu().numpy(), '%.4e', delimiter='\t')
-
-            for element, hidden_grad in saved_grads.items():
-                hidden_grad_file = os.path.join(hidden_folder, element + '.hidden_grad')
-                with open(hidden_grad_file, 'ab') as f:
-                    np.savetxt(f, hidden_grad.data.cpu().numpy(), '%.4e', delimiter='\t')
-
-        test_corr = util.pearson_corr(test_predict, predict_label_gpu)
-        print("Test correlation\t%s\t%.4f" % (model.root, test_corr))
-
-        np.savetxt(result_file + '.txt', test_predict.cpu().numpy(), '%.4e')
+        :param saved_grads: Dictionary containing the saved gradients.
+        :param hidden_folder: Directory to save the hidden layer gradients.
+        """
+        for element, hidden_grad in saved_grads.items():
+            hidden_grad_file = os.path.join(hidden_folder, f'{element}.hidden_grad')
+            with open(hidden_grad_file, 'ab') as f:
+                np.savetxt(f, hidden_grad.data.cpu().numpy(), '%.4e', delimiter='\t')
